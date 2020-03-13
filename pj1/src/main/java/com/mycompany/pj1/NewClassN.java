e5ca16f0951f9002364a697264cfef2cd6a81d15
อภิสรา ทองสังข์ <58841002+apisaraa@users.noreply.github.com>
2020-03-13 15:55:59 +0700

    qz

---
+++ pj1/src/main/java/com/mycompany/pj1/NewClassN.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pj1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class NewClassN {
+   public String name;
+   public String id;
+   public String address;
+   public String major;
+   public String phonenummber ;
+   public String academy ;
+   public String department ;
+}
